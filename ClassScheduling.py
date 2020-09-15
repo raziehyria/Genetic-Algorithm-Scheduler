2c1e5a20e69dbacdaac88c48f1940fc11e3d9cd5
iuh129 <iuh129@psu.edu>
2020-09-14 22:43:30 -0400

    Update Display Manager

--- ClassScheduling.py
+++ ClassScheduling.py
@@ -1,8 +1,10 @@
 from cs.config import Config
 from cs.geneticalgorithm import GeneticAlgorithm
 from cs.population import Population
+from cs.display import DisplayMgr
 
 config = Config()
+display_manager = DisplayMgr()
 
 population = Population(config.get_POPULATION_SIZE())
 genAlgo = GeneticAlgorithm(config)
@@ -14,10 +16,11 @@ while True:
     best_schedule = population.get_schedules()[0]
     best_schedule_fitness = best_schedule.get_fitness()
     if best_schedule_fitness >= 0.5:
-        print(best_schedule)
         break
     genAlgo.evolve(population)
     # population = population.get_schedules().sort(key=lambda x: x.get_fitness(), reverse=True)
     generationCount += 1
     print(best_schedule_fitness)
-print(generationCount)
+display_manager.print_available_data()
+display_manager.print_generation(population)
+display_manager.print_schedules_as_table(best_schedule)
