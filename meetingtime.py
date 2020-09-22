d20069c044b4642eee4ff98f72d9c9c5545a1317
Alec Goldenberg <ajg6148@psu.edu>
2020-09-22 16:21:19 -0400

    Added meeting time objects and list

--- meetingtime.py
+++ meetingtime.py
@@ -1,10 +1,16 @@
 class MeetingTime:
     """"""
 
-    def __init__(self, id, time):
-        self._id = id
+    def __init__(self, days, duration, time):
+        self._days = days
+        self._duration = duration
         self._time = time
 
-    def get_id(self): return self._id
+    def get_days(self): return self._days
 
     def get_time(self): return self._time
+
+    def get_duration(self): return self._duration
+
+    def __str__(self):
+        return "{} {}".format(self._days, self._time)
